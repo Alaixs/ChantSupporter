2cb6fc13727851db9dea5a960b9bc140a617aed9
Alexis <63883086+Alaixs@users.noreply.github.com>
2022-06-12 18:00:05 +0200

    Version 3.2

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-//version 2.7.b
-function playAudio(url) {
-  new Audio(url).play();
-}
-
 window.onload = () => {
 	let rgbText, nodes, hues;
 	
@@ -27,4 +22,70 @@ window.onload = () => {
 		});
 		window.requestAnimationFrame(loop);
 	})();
+};
+
+let audio = new Audio("https://github.com/Alaixs/ChantSupporter/raw/beta/Audio1.mp3");
+
+$('#ppb1').on("click",function(){
+  if($(this).hasClass('fa-play'))
+   {
+     $(this).removeClass('fa-play');
+     $(this).addClass('fa-pause');
+     audio.play();
+   }
+  else
+   {
+     $(this).removeClass('fa-pause');
+     $(this).addClass('fa-play');
+     audio.pause();
+   }
+});
+
+audio.onended = function() {
+     $("#ppb1").removeClass('fa-pause');
+     $("#ppb1").addClass('fa-play');
+};
+
+let audio1 = new Audio("https://github.com/Alaixs/ChantSupporter/raw/beta/Audio2.mp3");
+
+$('#ppb2').on("click",function(){
+  if($(this).hasClass('fa-play'))
+   {
+     $(this).removeClass('fa-play');
+     $(this).addClass('fa-pause');
+     audio1.play();
+   }
+  else
+   {
+     $(this).removeClass('fa-pause');
+     $(this).addClass('fa-play');
+     audio1.pause();
+   }
+});
+
+audio.onended = function() {
+     $("#ppb2").removeClass('fa-pause');
+     $("#ppb2").addClass('fa-play');
+};
+
+let audio2 = new Audio("https://github.com/Alaixs/ChantSupporter/raw/beta/Audio3.mp3");
+
+$('#ppb3').on("click",function(){
+  if($(this).hasClass('fa-play'))
+   {
+     $(this).removeClass('fa-play');
+     $(this).addClass('fa-pause');
+     audio2.play();
+   }
+  else
+   {
+     $(this).removeClass('fa-pause');
+     $(this).addClass('fa-play');
+     audio2.pause();
+   }
+});
+
+audio.onended = function() {
+     $("#ppb3").removeClass('fa-pause');
+     $("#ppb3").addClass('fa-play');
 };
